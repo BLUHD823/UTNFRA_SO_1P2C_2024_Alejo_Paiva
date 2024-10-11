5f842cd78a23b3a0b7dd24f275ee9cd4081f6c65
BLUHD823 <alejodeje@gmail.com>
2024-10-11 22:52:25 +0000

    fix: Ahora el script Punto_D ahora crea el sistema de directorios en el home del usuario

--- RTA_SCRIPT_Examen_20241008/Punto_D.sh
+++ RTA_SCRIPT_Examen_20241008/Punto_D.sh
@@ -1,2 +1,2 @@
-mkdir -p Estructura_Asimetrica/{cliente/cartas_{1..100},correo/{cartas_{1..100},cartero_{1..10}}}
-tree Estructura_Asimetrica/ --noreport | pr -T -s' ' -w 80 --column 4
+mkdir -p ~/Estructura_Asimetrica/{cliente/cartas_{1..100},correo/{cartas_{1..100},cartero_{1..10}}}
+tree ~/Estructura_Asimetrica/ --noreport | pr -T -s' ' -w 80 --column 4
