50f87738b2d8016a56b7734937d12af6658e4d0e
BLUHD823 <alejodeje@gmail.com>
2024-10-13 18:46:43 +0000

    fix: cambio en los permisos de la creacion del sistema de directorios

--- RTA_SCRIPT_Examen_20241008/Punto_A.sh
+++ RTA_SCRIPT_Examen_20241008/Punto_A.sh
@@ -1,3 +1,4 @@
 #!/bin/bash  
-mkdir -p /EXAMENES-UTN/{alumno_{1..3}/parcial_{1..3},profesores}
-tree /EXAMENES-UTN
+sudo snap install tree
+sudo mkdir -p /EXAMENES-UTN/{alumno_{1..3}/parcial_{1..3},profesores}
+sudo tree /EXAMENES-UTN
