a29f8187e1ac935e89c55da4585736a216c759dd
BLUHD823 <alejodeje@gmail.com>
2024-10-13 18:54:12 +0000

    fix: El script Punto_E ahora es capaz de crear la carpeta RTA_ARCHIVOS en caso de que no se encuentre en la maquina

--- RTA_SCRIPT_Examen_20241008/Punto_E.sh
+++ RTA_SCRIPT_Examen_20241008/Punto_E.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+mkdir - p ~/repogit/UTNFRA_SO_1P2C_2024_Alejo_Paiva/RTA_ARCHIVOS_Examen_20240929
 grep "MemTotal" /proc/meminfo > ~/repogit/UTNFRA_SO_1P2C_2024_Alejo_Paiva/RTA_ARCHIVOS_Examen_20240929/Filtro_Basico.txt
 sudo dmidecode -t chassis | grep -e "Chassis" -e "Manufacturer" >> ~/repogit/UTNFRA_SO_1P2C_2024_Alejo_Paiva/RTA_ARCHIVOS_Examen_20240929/Filtro_Basico.txt
 cat ~/repogit/UTNFRA_SO_1P2C_2024_Alejo_Paiva/RTA_ARCHIVOS_Examen_20240929/Filtro_Basico.txt
